292d9c2fe77db7c5ff8cee47118d44cdcbf42d0b
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-01-09 17:45:30 -0500

    Added hands to scene and wrote a script to animate them

--- Assets/Scripts/AnimateHandsOnTriggerInput.cs
+++ Assets/Scripts/AnimateHandsOnTriggerInput.cs
@@ -8,16 +8,27 @@ public class AnimateHandsOnTriggerInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty fistAnimationAction;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Hand animations will not work, add an animator");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (animator != null)
+        {
+            float indexTriggerValue = pinchAnimationAction.action.ReadValue<float>();
+            animator.SetFloat("Trigger", indexTriggerValue);
+            float handTriggerValue = fistAnimationAction.action.ReadValue<float>();
+            animator.SetFloat("Grip", handTriggerValue);
+        }
     }
 }
