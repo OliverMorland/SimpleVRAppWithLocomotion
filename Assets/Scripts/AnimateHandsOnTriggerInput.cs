acdb308221afcc516030b376e86beebe9ddc1e8d
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-01-09 17:32:07 -0500

    Imported Oculus Hands

---
+++ Assets/Scripts/AnimateHandsOnTriggerInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction;
+
+public class AnimateHandsOnTriggerInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty fistAnimationAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
